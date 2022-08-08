c02af8516d7018a174804e969c5ed3a9d8c427dd
daniel <daniil2705"gmail.com>
2022-08-08 12:02:37 +0300

    add entities

---
+++ src/main/java/com/dokl57/simplewarehouseapi/entity/Book.java
@@ -0,0 +1,52 @@
+package com.dokl57.simplewarehouseapi.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "title")
+    private String title;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "author")
+    private String author;
+
+    @NotNull
+    @Min(100)
+    @Column(name = "pages")
+    private int pages;
+
+    @NotNull
+    @Min(0)
+    @Column(name = "quantity")
+    private int quantity;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+
+}
---
+++ src/main/java/com/dokl57/simplewarehouseapi/entity/Genre.java
@@ -0,0 +1,46 @@
+package com.dokl57.simplewarehouseapi.entity;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "genres")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Genre genre = (Genre) o;
+        return name.equals(genre.name);
+    }
+
+    @Override
+    public int hashCode(){
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
